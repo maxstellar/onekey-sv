9f4628c803c0cb646e7721a2d645b1404c64b0ad
maxstellar <93678379+maxstellar@users.noreply.github.com>
2026-05-17 22:35:24 -0700

    chore: move og and twitter card (embeds) to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,24 @@
 
 <svelte:head>
 	<link rel="icon" type="image/x-icon" href="/favicon.ico" />
+	<meta property="og:author" content="onekey - Hack Club" />
+	<meta property="og:title" content="onekey" />
+	<meta property="og:type" content="website" />
+	<meta property="og:image" content="https://maxstellar.github.io/onekey/img/og-banner.png" />
+	<meta property="og:image:width" content="1200" />
+	<meta property="og:image:height" content="630" />
+	<meta
+		property="og:description"
+		content="make a program that only uses one key, get a one key macropad! A Hack Club program for teen coders."
+	/>
+	<meta name="twitter:site" content="onekey - Hack Club" />
+	<meta name="twitter:title" content="onekey" />
+	<meta
+		name="twitter:description"
+		content="make a program that only uses one key, get a one key macropad! A Hack Club program for teen coders."
+	/>
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:image:src" content="https://maxstellar.github.io/onekey/img/og-banner.png" />
 </svelte:head>
 
 {@render children()}
